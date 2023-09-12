39cfe7a246e4ff7ed3b7791ff0d13cf7b79553cd
alvator92 <alvator@list.ru>
2023-09-12 23:10:44 +0300

    init

---
+++ src/test/java/ru/education/springboot/SpringBootTraininigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.education.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTraininigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
