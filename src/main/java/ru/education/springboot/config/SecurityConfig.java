d9b82b7e0f4206ffdad265e6f35567f02b4e6643
alvator92 <alvator@list.ru>
2023-09-14 00:50:54 +0300

    UPDATE SpringSecurity with own login page, logic

--- src/main/java/ru/education/springboot/config/SecurityConfig.java
+++ src/main/java/ru/education/springboot/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.education.springboot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,6 +20,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailService = personDetailService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам spring security
+        // конфигурируем авторизацию
+        // name="username" name="password" ждет из формы
+//        http.csrf().disable()   // отключаем защиту от межсайтовой подделки запросов
+        http
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login");
+    }
+
+    // Настраиваем аутентификацию
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailService);
---
+++ src/main/java/ru/education/springboot/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package ru.education.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    private String LoginPage() {
+        return "auth/login";
+    }
+}
